1756ad361fa75a4b10dfedb2ecdd11ce08dbd3b1
caohuilin <1057275848@qq.com>
2018-02-10 19:53:38 +0800

    svg event bind

--- components/Defs.js
+++ components/Defs.js
@@ -4,12 +4,11 @@ import * as React from 'react'
 export default function() {
   return (
     <defs>
-      <filter x="-33.4%" y="-33.4%" width="166.9%" height="166.9%" filterUnits="objectBoundingBox" id="circle-filter-2">
-        <feMorphology radius="3.5" operator="dilate" in="SourceAlpha" result="shadowSpreadOuter1"></feMorphology>
-        <feOffset dx="0" dy="0" in="shadowSpreadOuter1" result="shadowOffsetOuter1"></feOffset>
-        <feGaussianBlur stdDeviation="10" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
-        <feComposite in="shadowBlurOuter1" in2="SourceAlpha" operator="out" result="shadowBlurOuter1"></feComposite>
-        <feColorMatrix values="0 0 0 0 0.2   0 0 0 0 0.329411765   0 0 0 0 0.611764706  0 0 0 0.5 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
+      <filter x="-52.5%" y="-52.5%" width="205.0%" height="205.0%" filterUnits="objectBoundingBox" id="filter-2">
+        <feMorphology radius="5" operator="dilate" in="SourceAlpha" result="shadowSpreadOuter1"/>
+        <feOffset dx="0" dy="0" in="shadowSpreadOuter1" result="shadowOffsetOuter1"/>
+        <feGaussianBlur stdDeviation="12.5" in="shadowOffsetOuter1" result="shadowBlurOuter1"/>
+        <feColorMatrix values="0 0 0 0 0.970775187   0 0 0 0 0.907238424   0 0 0 0 0.109462149  0 0 0 1 0" type="matrix" in="shadowBlurOuter1"/>
       </filter>
     </defs>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,73 @@
 import React from "react";
-import Defs from '../components/Defs'
-import smile from './smile'
+import Defs from "../components/Defs";
+import smile from "./smile";
 export default class extends React.Component {
+  shadow = null
+  smile = null
+  time = null
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovering: false
+    };
+  }
+  handleMouseOver = (e) => {
+    if (e.target === this.shadow && this.time) {
+      clearTimeout(this.time)
+    }
+    this.setState({ hovering: true });
+  };
+  handleMouseOut = (e) => {
+    if (e.target === this.smile) {
+      this.time = setTimeout(() => {
+        this.setState({ hovering: false })
+      }, 100);
+    } else {
+      this.setState({ hovering: false })
+    }
+  };
   render() {
     return (
       <div>
-        <svg id="chart" onClick={this.handleClick} ref={svg => this.svg = svg}>
-        <Defs />
-        <g className={this.nodeClassName} dangerouslySetInnerHTML={{__html: smile}} />
-        </svg> 
+        <svg
+          id="chart"
+          onClick={this.handleClick}
+          ref={svg => (this.svg = svg)}
+        >
+          <Defs />
+          {this.state.hovering && <circle
+              id="path-1"
+              cx="75"
+              cy="79"
+              r="73"
+              onMouseOver={this.handleMouseOver}
+              onMouseOut={this.handleMouseOut}
+              fill="transparent"
+              ref={(shadow) => this.shadow = shadow}
+          />}
+          {this.state.hovering && (
+            <circle
+              id="path-1"
+              cx="75"
+              cy="79"
+              r="50"
+              filter="url(#filter-2)"
+            />
+          )}
+          <circle id="path-1" fill="#FFFFFF" cx="75" cy="79" r="50" />
+          <g dangerouslySetInnerHTML={{ __html: smile }} />
+          <circle
+            id="path-1"
+            onMouseOver={this.handleMouseOver}
+            onMouseOut={this.handleMouseOut}
+            fill="transparent"
+            cx="75"
+            cy="79"
+            r="50"
+            ref={(smile) => this.smile = smile}
+          />
+        </svg>
       </div>
-    )
+    );
   }
 }
